cc844475f30d6d0bec1650f0366e41a4bc67501b
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-04-15 00:07:16 -0700

    edits 4/14

--- sketch.js
+++ sketch.js
@@ -1,47 +1,101 @@
 var s;
 var scl = 20;
-
 var food;
 
+let state = 'splash';
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  textAlign(CENTER);
+  textSize(20);
   s = new Snake();
   frameRate(5);
   pickLocation();
 }
-function pickLocation(){
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
+
+function pickLocation() {
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
 
 }
+
 function draw() {
-  background(51);
-  s.death();
-  s.update();
-  s.show();
-   if (s.eat(food)){
-     pickLocation();
-
-   }
-
-  fill(255,0,100);
-  rect(food.x, food.y, scl,scl);
-
+  switch (state) {
+    case ('splash'):
+      //execute splash keyCode
+      splashScreen();
+      break;
+    case ('game'):
+      game();
+      break;
+    case ('end'):
+      break;
+    default:
+      break;
+  }
 
 }
 
-function keyPressed(){
-  if (keyCode === UP_ARROW){
-    s.dir(0,-1);
-  } else if(keyCode === DOWN_ARROW){
-    s.dir(0,1);
-  } else if(keyCode === RIGHT_ARROW){
-    s.dir(1,0);
-  } else if(keyCode === LEFT_ARROW){
-    s.dir(-1,0);
+function game() {
+  background(137, 207, 240);
+  //  s.death();
+  s.update();
+  s.show();
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  fill(255, 0, 100);
+  noStroke();
+  triangle(food.x - 20, food.y, food.x, food.y + 28, food.x + 20, food.y);
+  ellipse(food.x + 9.5, food.y, scl, scl);
+  ellipse(food.x - 9.5, food.y, scl, scl);
+
+}
+
+function gameKeyPressed1() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  } else if (keyCode === '') {
+    splashScreen();
   }
 
+}
+
+function keyPressed() {
+  switch (state) {
+    case ('splash'):
+      //execute splash keyCode
+      splashScreenKeyPressed();
+      break;
+    case ('game'):
+      gameKeyPressed1();
+      break;
+    case ('end'):
+      break;
+    default:
+      break;
+  }
+}
+
+function splashScreen() {
+  background(0);
+  fill(255);
+  textSize(40);
+  text('There Are No Mistakes', width / 2, height / 7);
+  textSize(30);
+  text('Controls', width/2,height/ 3);
+  text("Arrow Keys To Move", width/2, height/2);
+  text('Spacebar To Begin',width/2, height/1.5);
+}
+
+function splashScreenKeyPressed() {
+  state = 'game';
 }
